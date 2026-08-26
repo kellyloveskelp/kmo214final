ae0f0812cc7a1fab4c07628171214e8d8a6818e4
Kelly Morgan <kellyloveskelp.com>
2026-08-26 14:55:23 -0700

    See #1

--- scratch/make_spagetti.R
+++ scratch/make_spagetti.R
@@ -7,157 +7,30 @@ cuenca2 <- read_csv("data/QuebradaCuenca2-Bisley.csv")
 cuenca3 <- read_csv("data/QuebradaCuenca3-Bisley.csv")
 puente_roto <- read_csv("data/RioMameyesPuenteRoto.csv")
 
-glimpse(cuenca2)
+glimpse(cuenca1)
 
-# Join all cuenca datasets 
+# Source moving-average function
+source("R/moving-average.R")
+
+# Join all cuenca datasets
 # binded_data <- bind_rows(cuenca1, cuenca2, cuenca3, puente_roto)
 # Not yet
 
-# Create tibble to average every 9 days
-cuenca1_smoothed <- tibble(
-  window_start = seq(
-    ymd("1984-05-20"),
-    ymd("2020-12-29"),
-    by = "9 days"
-  ),
-  Sample_ID = NA,
-  `NO3-N` = NA,
-  `NH4-N` = NA,
-  K = NA,
-  Mg = NA,
-  Ca = NA
-)
-
-cuenca2_smoothed <- tibble(
-  window_start = seq(
-    ymd("1984-05-20"),
-    ymd("2020-12-29"),
-    by = "9 days"
-  ),
-  Sample_ID = NA,
-  `NO3-N` = NA,
-  `NH4-N` = NA,
-  K = NA,
-  Mg = NA,
-  Ca = NA
-)
-
-cuenca3_smoothed <- tibble(
-  window_start = seq(
-    ymd("1984-05-20"),
-    ymd("2020-12-29"),
-    by = "9 days"
-  ),
-  Sample_ID = NA,
-  `NO3-N` = NA,
-  `NH4-N` = NA,
-  K = NA,
-  Mg = NA,
-  Ca = NA
-)
-
-roto_smoothed <- tibble(
-  window_start = seq(
-    ymd("1984-05-20"),
-    ymd("2020-12-29"),
-    by = "9 days"
-  ),
-  Sample_ID = NA,
-  `NO3-N` = NA,
-  `NH4-N` = NA,
-  K = NA,
-  Mg = NA,
-  Ca = NA
-)
-
-# Iterate through qs_smoothed by window start dates, aka every 9 days
-# Take the average of ion conc respectively given the sample dates within the window
-# We need to define which sample dates fall within the windows
-
-# CUENCA 1
-
-for (d in 1:nrow(cuenca1_smoothed)) {
-  # for some number from 1 to the number of rows in qs_smoothed
-  # What's the start and end of the window? Let's call it w1 and w2
-  w1 <- cuenca1_smoothed$window_start[d]
-  w2 <- w1 + 9
-  print(w1)
-  print(w2)
-  data_window <- filter(cuenca1, (Sample_Date >= w1 & Sample_Date < w2))
-  print(data_window)
-  #cuenca1_smoothed$Sample_ID[d] <- first(data_window$Sample_ID)
-  cuenca1_smoothed$`NO3-N`[d] <- mean(data_window$`NO3-N`, na.rm = TRUE)
-  cuenca1_smoothed$`NH4-N`[d] <- mean(data_window$`NH4-N`, na.rm = TRUE)
-  cuenca1_smoothed$K[d] <- mean(data_window$K, na.rm = TRUE)
-  cuenca1_smoothed$Mg[d] <- mean(data_window$Mg, na.rm = TRUE)
-  cuenca1_smoothed$Ca[d] <- mean(data_window$Ca, na.rm = TRUE)
-} 
-cuenca1_smoothed$Sample_ID <- first(cuenca1$Sample_ID)
-
-# CUENCA 2
-
-for (d in 1:nrow(cuenca2_smoothed)) {
-  # for some number from 1 to the number of rows in qs_smoothed
-  # What's the start and end of the window? Let's call it w1 and w2
-  w1 <- cuenca2_smoothed$window_start[d]
-  w2 <- w1 + 9
-  print(w1)
-  print(w2)
-  data_window <- filter(cuenca2, (Sample_Date >= w1 & Sample_Date < w2))
-  print(data_window)
-  cuenca2_smoothed$`NO3-N`[d] <- mean(data_window$`NO3-N`, na.rm = TRUE)
-  cuenca2_smoothed$`NH4-N`[d] <- mean(data_window$`NH4-N`, na.rm = TRUE)
-  cuenca2_smoothed$K[d] <- mean(data_window$K, na.rm = TRUE)
-  cuenca2_smoothed$Mg[d] <- mean(data_window$Mg, na.rm = TRUE)
-  cuenca2_smoothed$Ca[d] <- mean(data_window$Ca, na.rm = TRUE)
-}
-cuenca2_smoothed$Sample_ID <- first(cuenca2$Sample_ID)
-
-# CUENCA 3
-
-for (d in 1:nrow(cuenca3_smoothed)) {
-  # for some number from 1 to the number of rows in qs_smoothed
-  # What's the start and end of the window? Let's call it w1 and w2
-  w1 <- cuenca3_smoothed$window_start[d]
-  w2 <- w1 + 9
-  print(w1)
-  print(w2)
-  data_window <- filter(cuenca3, (Sample_Date >= w1 & Sample_Date < w2))
-  print(data_window)
-  cuenca3_smoothed$`NO3-N`[d] <- mean(data_window$`NO3-N`, na.rm = TRUE)
-  cuenca3_smoothed$`NH4-N`[d] <- mean(data_window$`NH4-N`, na.rm = TRUE)
-  cuenca3_smoothed$K[d] <- mean(data_window$K, na.rm = TRUE)
-  cuenca3_smoothed$Mg[d] <- mean(data_window$Mg, na.rm = TRUE)
-  cuenca3_smoothed$Ca[d] <- mean(data_window$Ca, na.rm = TRUE)
-}
-cuenca3_smoothed$Sample_ID <- first(cuenca3$Sample_ID)
-
-# PUENTE ROTO
-
-for (d in 1:nrow(roto_smoothed)) {
-  # for some number from 1 to the number of rows in qs_smoothed
-  # What's the start and end of the window? Let's call it w1 and w2
-  w1 <- roto_smoothed$window_start[d]
-  w2 <- w1 + 9
-  print(w1)
-  print(w2)
-  data_window <- filter(puente_roto, (Sample_Date >= w1 & Sample_Date < w2))
-  print(data_window)
-  roto_smoothed$`NO3-N`[d] <- mean(data_window$`NO3-N`, na.rm = TRUE)
-  roto_smoothed$`NH4-N`[d] <- mean(data_window$`NH4-N`, na.rm = TRUE)
-  roto_smoothed$K[d] <- mean(data_window$K, na.rm = TRUE)
-  roto_smoothed$Mg[d] <- mean(data_window$Mg, na.rm = TRUE)
-  roto_smoothed$Ca[d] <- mean(data_window$Ca, na.rm = TRUE)
-}
-roto_smoothed$Sample_ID <- first(puente_roto$Sample_ID)
+mov_avg_cuenca1 <- moving_average(cuenca1)
+mov_avg_cuenca2 <- moving_average(cuenca2)
+mov_avg_cuenca3 <- moving_average(cuenca3)
+mov_avg_roto <- moving_average(puente_roto)
 
 # Now bind data
-binded_data <- bind_rows(
-  cuenca1_smoothed, cuenca2_smoothed, cuenca3_smoothed, roto_smoothed
+bind_streams <- bind_rows(
+  mov_avg_cuenca1,
+  mov_avg_cuenca2,
+  mov_avg_cuenca3,
+  mov_avg_roto
 )
 
 # Pivot data in cuenca1 for visuallization
-pivot_data <- binded_data |>
+pivot_streams <- bind_streams |>
   pivot_longer(
     cols = c(`NO3-N`, `NH4-N`, K, Mg, Ca),
     names_to = "Ions",
@@ -167,7 +40,7 @@ pivot_data <- binded_data |>
 # Make a plot of concentration over time
 
 ggplot(
-  data = pivot_data,
+  data = pivot_streams,
   mapping = aes(
     x = window_start,
     y = Concentration,
@@ -175,12 +48,10 @@ ggplot(
   )
 ) +
   geom_line() +
-  theme_classic(
-  ) +
+  theme_classic() +
   labs(
     title = "Ion Concentration over Time",
     x = "Sampling Dates",
     y = "Concentration of Ion (mg/l)"
   ) +
-  facet_wrap(~Ions, scales = "free")
-
+  facet_wrap(~Ions, nrow = 5, ncol = 1, scales = "free")
